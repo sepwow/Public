07d12b18b01b9acf2f6f19a0b4c83e1e0730caae
Dmitrii Galanov <106379811+sepwow@users.noreply.github.com>
2022-11-04 15:10:35 +0700

    Currency Converter Telegram Bot

---
+++ Currency Convert Telegram Bot/config.py
@@ -0,0 +1,7 @@
+TOKEN = '5543768980:AAFY4PDlkLtuxXME_OtG9cI5QoOxkNMCRB0'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ Currency Convert Telegram Bot/extensions.py
@@ -0,0 +1,40 @@
+import json
+import requests
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class CurrencyConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        quote = quote.lower()
+        base = base.lower()
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        total_base *= amount
+
+        return round(total_base, 2)
